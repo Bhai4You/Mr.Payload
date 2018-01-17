59e3e2a2dbc8acf4f5741d69ed385756ddb561ae
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-01-17 14:13:45 +0530

    run first...!

---
+++ requirement.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Run \e[32m ruby mr-payload.rb \e[0m\e[1m...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Sutariya Parixit (8h4i)"
+echo
+echo
