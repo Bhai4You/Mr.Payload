ab049bf659844024e0aabf7f5203c823bdd22fc8
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-01-17 08:38:15 +0000

    Create logo.rb

---
+++ logo/logo.rb
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/bin/ruby
+=begin
+Everything Is Possible. Let's Do!!!!
+This is My First Program.. Don't Fear I Am NooB !!!
+
+
+Name	        :   Mr.Payload
+Code	        :   Ruby
+sec.code      :   8h4i
+Site.              :   http://bhai4you.blogspot.com
+Coder           :   Sutariya Parixit
+Github          :   https://github.com/Bhai4You
+
+
+]=====> Don't Copy Or Modify...<=====[
+
+
+=end
+exec ("figlet -f big '        Mr. Payload' | lolcat")
